e2bd4be67cff668b6ced6ce2193963b59f63b31a
admin <admin@YD02-21>
2024-07-10 11:55:29 +0900

    수정 및 삭제기능 추가

--- SManager/src/co/yedam/control/StudentControl.java
+++ SManager/src/co/yedam/control/StudentControl.java
@@ -12,16 +12,16 @@ import co.yedam.vo.StudentVO;
 public class StudentControl {
 	Scanner scanner = new Scanner(System.in);
 	StudentDAO sdao = new StudentDAO();
-	
+
 	public void run() {
 		boolean isTrue = true;
-		
-		while(isTrue) {
+
+		while (isTrue) {
 			System.out.println("1.학생목록  2.등록  3.수정  4.삭제  5.종료");
 			System.out.println("선택> ");
 			int menu = Integer.parseInt(scanner.nextLine());
-			
-			switch(menu) {
+
+			switch (menu) {
 			case 1:
 				studentList();
 				break;
@@ -29,57 +29,102 @@ public class StudentControl {
 				addStudent();
 				break;
 			case 3:
+				modifyStudent();
 				break;
 			case 4:
+				removeStudent();
 				break;
 			case 5:
 				System.out.println("종료합니다.");
 				isTrue = false;
 			}
 		}
-	} //end of run
-	
-	//등록 기능.
-	void addStudent() {
-		System.out.println(">학생번호 입력.");
+	} // end of run
+
+	// 삭제 기능.
+	void removeStudent() {
+		System.out.println("삭제할 학생번호>   [예) 'S2024-00']");
 		String sno = scanner.nextLine();
+		if (sdao.removeStudent(sno) == 1) {
+			System.out.println("삭제완료!");
+		} else {
+			System.out.println("삭제처리 중 문제발생!");
+		}
 		
-		System.out.println(">학생 생년월일 입력");
-		String stbirth = scanner.nextLine();
-		
-		System.out.println(">학생 주소 입력");
-		String staddr = scanner.nextLine();
-		
-		System.out.println(">학생이름 입력.");
-		String sname = scanner.nextLine();
-		
-		System.out.println(">학생연락처 입력.");
+	}//end of removeStudent
+
+	// 수정 기능.
+	void modifyStudent() {
+		String sno = ""; // 블럭레벨 변수.
+		while (true) {
+			System.out.println("변경할 학생번호>   [예) 'S2024-00']");
+			sno = scanner.nextLine();
+			if (sdao.selectExists(sno) == 1) {
+				// 학생번호 존재한다는 의미.
+				break;
+			}
+			System.out.println("찾는 학생번호가 없음 학생번호 다시 입력>");
+		}
+
+		System.out.println("변경할 연락처>");
 		String phon = scanner.nextLine();
 		
+		System.out.println("변경할 주소>");
+		String staddr = scanner.nextLine();
+		
+		StudentVO std = new StudentVO();
+		std.setStdNo(sno);
+		std.setStdPhone(phon);
+		std.setAddress(staddr);
+//		std.setStdName(sname);
+//		std.setBirthDate(stbirth);
+
+		if (sdao.updateStudent(std)) {
+			System.out.println("수정완료!");
+		}
+
+	}
+
+	// 등록 기능.
+	void addStudent() {
+		System.out.println(">학생번호 입력.   [예) 'S2024-00']");
+		String sno = scanner.nextLine();
+
+		System.out.println(">학생 생년월일 입력.   [예) '2024-00-00']");
+		String stbirth = scanner.nextLine();
+
+		System.out.println(">학생 주소 입력.");
+		String staddr = scanner.nextLine();
+
+		System.out.println(">학생이름 입력.");
+		String sname = scanner.nextLine();
+
+		System.out.println(">학생연락처 입력.   [예) '000-0000-0000']");
+		String phon = scanner.nextLine();
+
 		StudentVO std = new StudentVO();
 		std.setStdNo(sno);
 		std.setStdName(sname);
 		std.setStdPhone(phon);
 		std.setAddress(staddr);
 		std.setBirthDate(stbirth);
-		
-		//입력기능 호출.
-		if(sdao.insertStudent(std)) {
+
+		// 입력기능 호출.
+		if (sdao.insertStudent(std)) {
 			System.out.println("저장완료!");
-		}else {
+		} else {
 			System.out.println("처리 중 예외발생!");
 		}
-		
+
 	}
-	
-	
-	//목록 출력 기능.
+
+	// 목록 출력 기능.
 	void studentList() {
 		List<StudentVO> students = sdao.selectList();
 		System.out.println("학생번호     학생이름     연락처");
 		System.out.println("-------------------------------------");
-		for(StudentVO svo : students) {
+		for (StudentVO svo : students) {
 			System.out.println(svo.briefShow());
 		}
-	}//end of studentList().
+	}// end of studentList().
 }// end class
--- SManager/src/co/yedam/dao/StudentDAO.java
+++ SManager/src/co/yedam/dao/StudentDAO.java
@@ -1,6 +1,8 @@
 package co.yedam.dao;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +14,50 @@ import co.yedam.vo.StudentVO;
  */
 
 public class StudentDAO extends DAO {
+	
+	//단건조회.
+	public int selectExists(String sno) {
+		String sql = "select count(1) from tbl_student";
+		sql += "      where std_no = ?";
+		conn = getConn();
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, sno);
+			rs = psmt.executeQuery();
+			if(rs.next()) {
+			   return rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	//수정기능
+	public boolean updateStudent(StudentVO svo) {
+		String sql = "update tbl_stuednt";
+		sql += "   set    std_phone = ?";
+		sql += "           ,address = ?";
+		sql += "   where     std_no = ?";
+		conn = getConn();
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, svo.getStdPhone());
+			psmt.setString(2, svo.getAddress());
+			psmt.setString(3, svo.getStdNo());
+			
+			int r = psmt.executeUpdate(); // 쿼리실행.
+			if (r == 1) {
+				return true; //정상처리.
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false; //비정상처리.
+		
+	} // end of updateStudent.
+	
 	
 	//등록기능.
 	public boolean insertStudent(StudentVO svo) {
@@ -35,7 +81,7 @@ public class StudentDAO extends DAO {
 			e.printStackTrace();
 		}
 		return false; //비정상처리.
-	}
+	} //end of insertStudent.
 	
 	//목록반환기능.
 	public List<StudentVO> selectList() {
@@ -63,6 +109,25 @@ public class StudentDAO extends DAO {
 		}
 		return list;
 	}// end of selectList().
+	
+	/*delete from tbl_student
+	where std_no = '2024-04';*/
+	
+	//삭제기능.
+	public int removeStudent(String stdNo) {
+		conn = getConn();
+		String sql = "DELETE tbl_student";
+		sql += " where std_no = '" + stdNo + "'";
+		
+		try {
+			Statement stmt = conn.createStatement();
+			int r = stmt.executeUpdate(sql); //insert, update, delete => 처리된 건수를 반환해주는 메소드
+			return r;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 	
 	//DB를 처리하는 곳
 	// insert update delete select 등등
